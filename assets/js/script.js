5c49896f6f3a7cf598447e3993a345398849bc02
lorenzo-neri <lorenzo21neri@gmail.com>
2023-09-12 15:34:54 +0200

    add carousel main img

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,28 +3,58 @@ Partendo dal markup della versione svolta in js plain, rifare lo slider ma quest
 Bonus: 1- al click su una thumb, visualizzare in grande l'immagine corrispondente 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
 */
 
-const slides = [
-{
-    image: './assets/img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: './assets/img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
-        image: './assets/img/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-        image: './assets/img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-        image: './assets/img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
-];
+const { createApp } = Vue
 
-console.log(slides);
+createApp({
+    data() {
+        return {
+            activeImage: 0,
+            slides: [
+                {
+                    image: './assets/img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                }, {
+                    image: './assets/img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                }, {
+                    image: './assets/img/03.webp',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                }, {
+                    image: './assets/img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                }, {
+                    image: './assets/img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                }
+            ],
+        }
+    },
+    methods: {
+        next() {
+            console.log(this.next);
+            console.log(this.activeImage);
+
+            this.activeImage++;
+            
+            console.log(this.slides); //Proxy
+
+            if (this.activeImage > this.slides.length - 1) {
+                this.activeImage = 0
+            }
+            
+        },
+        prev() {
+            console.log(this.prev);
+            this.activeImage--;
+            if (this.activeImage < 0) {
+                this.activeImage = this.slides.length - 1
+            }
+        }
+    }
+
+}).mount('#app')
